77e17211a2c58a82df0eb0ed5143a6ee91a44269
Syed-Ather-Ul-Hassan <syedatherulhassan@gmail.com>
2024-04-01 16:13:59 +0500

    Minor change in text

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inqirer from "inquirer";
 const answer = await inqirer.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter second number", type: "number", name: "secondNumber" },
+    { message: "Enter your first number", type: "number", name: "firstNumber" },
+    { message: "Enter your second number", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operators to perform operation",
         type: "list",
